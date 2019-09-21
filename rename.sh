d83ef86b13b3a6cad152943ea417e209c7072047
Zhang Junyu <junyu92@163.com>
2019-09-22 02:22:23 +0800

    rename

---
+++ rename.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-d <string>] [-t <string>]" 1>&2; exit 1; }
+
+while getopts ":d:t:" o; do
+    case "${o}" in
+        d)
+            DIC_POSITION=${OPTARG}
+            ;;
+        t)
+            TARGET_NAME=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${DIC_POSITION}" ] || [ -z "${TARGET_NAME}" ]; then
+    usage
+fi
+
+count=1
+find ${DIC_POSITION} -type f | grep -v 'DS_Store$' | while read LINE; do
+	file=$LINE
+	ext=${file##*.}
+	src="${file}"
+	tar="$(dirname "${file}")/${TARGET_NAME}_$count.$ext"
+	mv "$src" "$tar"
+	let "count++";
+done
